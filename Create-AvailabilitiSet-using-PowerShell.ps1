4d17c330747196f1a1bdcdb34d45c0fd80be2e76
Macnair Rios Calixto <78324420+Macnair-Rios-Calixto@users.noreply.github.com>
2021-02-01 23:16:49 +0300

    Add files via upload

---
+++ Create-AvailabilitiSet-using-PowerShell.ps1
@@ -0,0 +1,32 @@
+# Create a Resource Group
+New-AzResourceGroup `
+   -Name RG-AVSET `
+   -Location EastUS2
+
+#Create a AvailabilitySet
+New-AzAvailabilitySet `
+   -Location "EastUS2" `
+   -Name "AVSet-FS" `
+   -ResourceGroupName "RG-AVSET" `
+   -Sku aligned `
+   -PlatformFaultDomainCount 2 `
+   -PlatformUpdateDomainCount 2  
+
+#Create Credentials
+$cred = Get-Credential
+
+#Create Virtual Machines
+for ($i=1; $i -le 2; $i++)
+{
+    New-AzVm `
+        -ResourceGroupName "RG-AVSET" `
+        -Name "FS$i" `
+        -Location "East US2" `
+        -VirtualNetworkName "Vnet-AVSet" `
+        -SubnetName "Subnet" `
+        -SecurityGroupName "Nsg-AVSet" `
+        -PublicIpAddressName "Pip-AVSet$i" `
+        -AvailabilitySetName "AVSet-FS" `
+        -Credential $cred
+}
+
